cbbcc349b4085ed2bf7458dd9ba7d476090917fd
Yash Shirish Mehta <yashmehta2711@gmail.com>
2023-03-13 22:54:21 -0700

    added 5 new changes to the pizza party app for lab 2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CS646 Lab 1 Pizza Party App
 <img src="screenshots/1.png" alt="">
+
+# CS646 Lab 2 Pizza Party App Updated
+<img src="screenshots/1_1.png" alt="">
--- app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -1,4 +1,7 @@
 
+
+
+
 package com.zybooks.pizzaparty
 
 import android.os.Bundle
@@ -9,42 +12,63 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.ceil
 
-const val SLICES_PER_PIZZA = 8
-/*
-MiniActivity class has the application logic which is considered as controller of the app.
- */
-
+//const val SLICES_PER_PIZZA = 8
+private const val KEY_TOTAL_PIZZAS = "totalPizzas"
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var numAttendEditText: EditText
-    private lateinit var numPizzasTextView: TextView
-    private lateinit var howHungryRadioGroup: RadioGroup
-
+    private lateinit var numAttendEditText: EditText //This is for how many people are eating.
+    private lateinit var numPizzasTextView: TextView //This is for how many pizzas are needed.
+    private lateinit var howHungryRadioGroup: RadioGroup //This is for how hungry the people are.
+    private var totalPizzas = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         numAttendEditText = findViewById(R.id.num_attend_edit_text)
         numPizzasTextView = findViewById(R.id.num_pizzas_text_view)
         howHungryRadioGroup = findViewById(R.id.hungry_radio_group)
+
+        // Restore state
+        if (savedInstanceState != null) {
+            totalPizzas = savedInstanceState.getInt(KEY_TOTAL_PIZZAS)
+            displayTotal()
+        }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_TOTAL_PIZZAS, totalPizzas)
+    }
+    /**
+     * This function/method calculates the number of pizzas needed based on the number of people
+     * attending the party as well as how hungry they are.
+     */
     fun calculateClick(view: View) {
 
         // Get the text that was typed into the EditText
         val numAttendStr = numAttendEditText.text.toString()
 
         // Convert the text into an integer
-        val numAttend = numAttendStr.toInt()
+        val numAttend = numAttendStr.toIntOrNull() ?: 0
 
-        // Determine how many slices on average each person will eat
-        val slicesPerPerson = when (howHungryRadioGroup.checkedRadioButtonId) {
-            R.id.light_radio_button -> 2
-            R.id.medium_radio_button -> 3
-            else -> 4
+        // Get hunger level selection
+        val hungerLevel = when (howHungryRadioGroup.getCheckedRadioButtonId()) {
+            R.id.light_radio_button -> PizzaCalculator.HungerLevel.LIGHT
+            R.id.medium_radio_button -> PizzaCalculator.HungerLevel.MEDIUM
+            else -> PizzaCalculator.HungerLevel.RAVENOUS
         }
 
-        // Calculate and show the number of pizzas needed
-        val totalPizzas = ceil(numAttend * slicesPerPerson / SLICES_PER_PIZZA.toDouble()).toInt()
-        numPizzasTextView.text = "Total pizzas: $totalPizzas"
+        // Get the number of pizzas needed
+        val calc = PizzaCalculator(numAttend, hungerLevel)
+        totalPizzas = calc.totalPizzas
+        displayTotal()
+
+        // Place totalPizzas into the string resource and display
+        //val totalText = getString(R.string.total_pizzas_num, totalPizzas)
+        //numPizzasTextView.setText(totalText)
+    }
+
+    private fun displayTotal() {
+        val totalText = getString(R.string.total_pizzas_num, totalPizzas)
+        numPizzasTextView.text = totalText
     }
 }
